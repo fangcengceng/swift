cd44f6d551c48caf822a41661f061b682ecb5b4e
CengCeng.Fang <fangcengceng@bjxmail.com>
2017-07-20 15:58:35 +0800

    添加一个工程

---
+++ 一个工程多个项目/一个工程多个项目/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  一个工程多个项目
+//
+//  Created by 北极星 on 2017/7/20.
+//  Copyright © 2017年 aa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
