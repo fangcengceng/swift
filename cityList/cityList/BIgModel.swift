ccf8f9640d579f096cd58d5cb3d17335f7d8ed1b
CengCeng.Fang <fangcengceng@bjxmail.com>
2017-08-02 14:52:57 +0800

    wu

---
+++ cityList/cityList/BIgModel.swift
@@ -0,0 +1,23 @@
+//
+//  BIgModel.swift
+//  cityList
+//
+//  Created by 北极星 on 2017/7/10.
+//  Copyright © 2017年 aa. All rights reserved.
+//
+
+import UIKit
+
+class BIgModel: NSObject {
+    
+    var parentID: NSInteger? = 0
+    var citysArray = [CityModel]()
+    
+    override init() {
+        super.init()
+
+    }
+    
+
+    
+}
